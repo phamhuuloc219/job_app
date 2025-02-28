cf2f4f5e226389f36d2e9ce7c4519341be6f54ab
phamhuuloc219 <phamhuuloc2192003@gmail.com>
2025-02-28 11:12:22 +0700

    feat: setup project

---
+++ lib/controllers/signup_provider.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:job_app/constants/app_constants.dart';
+import 'package:job_app/models/request/auth/signup_model.dart';
+import 'package:job_app/services/helpers/auth_helper.dart';
+
+class SignUpNotifier extends ChangeNotifier {
+  
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,57 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:job_app/screen/home_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:job_app/constants/app_constants.dart';
+import 'package:job_app/controllers/image_provider.dart';
+import 'package:job_app/controllers/jobs_provider.dart';
+import 'package:job_app/controllers/login_provider.dart';
+import 'package:job_app/controllers/onboarding_provider.dart';
+import 'package:job_app/controllers/profile_provider.dart';
+import 'package:job_app/controllers/signup_provider.dart';
+import 'package:job_app/controllers/zoom_provider.dart';
+import 'package:job_app/views/screens/mainscreen.dart';
+import 'package:job_app/views/screens/onboarding/onboarding_screen.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+// Widget defaultHome = const OnboardingScreen();
+Widget defaultHome = const Mainscreen();
+///TODO: Hook the app to firebase using firebase cli
+void main() async {
+ 
+
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (context) => OnBoardNotifier()),
+    ChangeNotifierProvider(create: (context) => LoginNotifier()),
+    ChangeNotifierProvider(create: (context) => ZoomNotifier()),
+    ChangeNotifierProvider(create: (context) => SignUpNotifier()),
+    ChangeNotifierProvider(create: (context) => JobsNotifier()),
+    ChangeNotifierProvider(create: (context) => ImageUpoader()),
+    ChangeNotifierProvider(create: (context) => ProfileNotifier()),
+  ], child: const MyApp()));
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'NTU Student Job Market Application',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: HomeScreen(),
-    );
+    return ScreenUtilInit(
+        useInheritedMediaQuery: true,
+        designSize: const Size(375, 812),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'NTU Student Job Market Application',
+            theme: ThemeData(
+              scaffoldBackgroundColor: Color(kLight.value),
+              iconTheme: IconThemeData(color: Color(kDark.value)),
+              primarySwatch: Colors.grey,
+            ),
+            home: defaultHome,
+          );
+        });
   }
 }
