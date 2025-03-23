cff20125023ef345f426ede9e4c71605b998f1ab
phamhuuloc219 <phamhuuloc2192003@gmail.com>
2025-03-24 00:34:12 +0700

    feat: coding recently posted(company name, job name, salary)

--- lib/views/screens/job/widgets/job_vertical_title.dart
+++ lib/views/screens/job/widgets/job_vertical_title.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:get/get.dart';
 import 'package:job_app/constants/app_constants.dart';
 import 'package:job_app/models/response/jobs/jobs_response.dart';
+import 'package:job_app/views/common/exports.dart';
+import 'package:job_app/views/screens/job/job_details_screen.dart';
 
 class JobVerticalTitle extends StatelessWidget {
   const JobVerticalTitle({super.key, required this.job});
@@ -12,17 +16,19 @@ class JobVerticalTitle extends StatelessWidget {
   Widget build(BuildContext context) {
     return FittedBox(
       child: GestureDetector(
-        onTap: () {},
+        onTap: () {
+          Get.to(()=> JobDetailsScreen(id: job.id, title: job.title, companyName:  job.companyName));
+        },
         child: Padding(
           padding: const EdgeInsets.only(bottom: 8.0),
           child: Container(
-            height: height * 0.1,
+            padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.w),
+            height: height * 0.11,
             width: width,
             decoration: BoxDecoration(
               color: Color(kLightGrey.value),
               borderRadius: BorderRadius.all(Radius.circular(9.w)),
             ),
-
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -35,11 +41,52 @@ class JobVerticalTitle extends StatelessWidget {
                         CircleAvatar(
                           radius: 30,
                           backgroundImage: NetworkImage(job.imageUrl),
-                        )
+                        ),
+
+                        SizedBox(width: 10.w,),
+
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ReusableText(
+                                text: job.companyName,
+                                style: appStyle(
+                                    12,
+                                    Color(kDark.value),
+                                    FontWeight.w500
+                                ),
+                            ),
+                            SizedBox(
+                              width: width * 0.5,
+                              child: ReusableText(
+                                text: job.title,
+                                style: appStyle(
+                                    12,
+                                    Color(kDarkGrey.value),
+                                    FontWeight.w500
+                                ),
+                              ),
+                            ),
+                            ReusableText(
+                              text: "${job.salary} per ${job.period}",
+                              style: appStyle(
+                                  12,
+                                  Color(kDarkGrey.value),
+                                  FontWeight.w500
+                              ),
+                            ),
+                          ],
+                        ),
                       ],
-                    )
+                    ),
+                    CircleAvatar(
+                      radius: 18,
+                      backgroundColor: Color(kLight.value),
+                      child: Center(child: Icon(Ionicons.chevron_forward)),
+                    ),
                   ],
-                )
+                ),
               ],
             ),
           ),
